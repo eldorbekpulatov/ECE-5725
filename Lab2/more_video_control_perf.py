dc4b98106fa16c696e2dcc5d1e94ba42fedc8576
Eldor Bekpulatov <eb654@cornell.edu>
2019-10-03 18:40:14 -0400

    lab2 part2 completed

--- Lab2/more_video_control_perf.py
+++ Lab2/more_video_control_perf.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import time
 import os
 
+start = time.time()
+
 GPIO.setmode(GPIO.BCM)   # Set for broadcom numbering not board numbers...
 
 # Pin 17 - pause
@@ -17,10 +19,8 @@ GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(19, GPIO.IN)
 GPIO.setup(26, GPIO.IN)
 
-pressed = False
-
-while True:
-    time.sleep(0.2)  # Without sleep, no screen output!
+while (time.time() - start) < 10:
+    #time.sleep(0.00002)  # Without sleep, no screen output!
     if ( not GPIO.input(17) ):
         os.system('echo "pause" > $(pwd)/video_fifo')
     elif ( not GPIO.input(19) ):
